8f083e4a36983d3f9768341ecea9a8fb38d72ba5
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2019-08-02 22:44:12 +0200

    [D2 Web] : update view.

--- diablo-items/src/Items/ItemViewer.js
+++ diablo-items/src/Items/ItemViewer.js
@@ -1,10 +1,9 @@
 import React, { FunctionComponent } from "react";
-import { MDBDataTable } from 'mdbreact';
+import { MDBDataTable, MDBRow } from 'mdbreact';
 import { orderBy } from 'lodash'
 import * as style from './ItemViewer.css'
 
-class ItemViewer extends React.Component
-{
+class ItemViewer extends React.Component {
     constructor(props) {
         super(props);
         console.log(this.props);
@@ -12,63 +11,48 @@ class ItemViewer extends React.Component
 
     render()
     {
-        const itemData = this.props.Items.map(function(item) {
-            return {
-                LevelRequired: item.LevelRequired,
-                Name: item.Name,
-                Category: item.Category,
-                SubCategory: item.SubCategory,
-                Properties:
-                    orderBy(item.Properties, ['Minimum'],['desc'])
-                    .map(property => {
-
-                    var value = property.Minimum == property.Maximum ? `${property.Minimum}` :
-                                `[${property.Minimum}-${property.Maximum}]`;
-
-                    return <div  className="diablo-attribute">+{value} {property.Name}</div>
-                })
-
-            }
-        });
-
-        const data =
+        // 3) revoir l’affichage : fond sombre
+        // rendu en flexbox
+        // - les propriétés de l’objet en bleu : utiliser isPercent
+        return (orderBy(this.props.Items, ['Name']).map(function(item)
         {
-            columns: [
+            const attributes =
+                orderBy(item.Properties, ['Minimum'],['desc'])
+                .map(property =>
                 {
-                    label: 'Level Required',
-                    field: 'LevelRequired',
-                    sort: 'asc',
-                    width: 50
-                },
-                {
-                    label: 'Name',
-                    field: 'Name',
-                    sort: 'asc',
-                    width: 90
-                },
-                {
-                    label: 'Category',
-                    field: 'Category',
-                    sort: 'asc',
-                    width: 50
-                },
-                {
-                    label: 'Sub Category',
-                    field: 'SubCategory',
-                    sort: 'asc',
-                    width: 80
-                },
-                {
-                    label: 'Properties',
-                    field: 'Properties',
-                    width: 200
-                },
-            ],
-            rows : itemData
-        }
-        return (<MDBDataTable striped bordered small data={data} />
-    );
-    }
-};
+                    var value = (property.Minimum == property.Maximum ? `${property.Minimum}` :
+                        `[${property.Minimum}-${property.Maximum}]`)
+                    var isPercent = (property.IsPercent ? '%' : '')
 
+                    return <div  className="diablo-attribute">+{value}{isPercent} {property.Name}</div>
+                });
+
+            return(
+            <>
+           <div className="item">
+
+           OBJECT IMAGE
+            <div style={{color : "orange"}}> {/*qualité lié à la qualité e l'objet et revoit la couleur */}
+          {item.Name} <br/>
+          {item.Type} {/* Utiliser le type de l'objet plutôt */}
+          </div>
+          <div style={{color : "white"}}>{/*revoir la couleur*/}
+                {item.MaximumDefenseMinimum > 0 ? <div>Defense : <span className="diablo-attribute">{item.MaximumDefenseMinimum}-{item.MaximumDefenseMaximum}</span></div> : ''}
+                {item.MinimumTwoHandedDamageMinimum > 0 ? <div>Two-Hand Damage : <span className="diablo-attribute">{item.MinimumTwoHandedDamageMinimum}-{item.MaximumTwoHandedDamageMinimum} to {item.MinimumTwoHandedDamageMaximum}-{item.MaximumTwoHandedDamageMaximum}</span></div> : ''}
+            {/* - one-handed damage ou two-handed damage ou les deux : dommage avec le bonus de dommage compris (min damage, Max damage, pourcentage, d’image)
+         - défense avec le bonus de défense (défense , defence pourcent )*/}
+          </div>
+          <div style={{color : "red"}}>{/*  rvoir la couleur */}
+            {/* - stats requis, level requis en rouge */ }
+                {item.StrengthRequired > 0 ? <div>Required Strength : {item.StrengthRequired} </div> : ''}
+                {item.DexterityRequired > 0 ? <div>Required Dexterity : {item.DexterityRequired} </div> : ''}
+                {item.LevelRequired > 0 ? <div>Required Level : {item.LevelRequired} </div> : ''}
+          </div>
+          <div style={{color : "#4545a4"}}>{/* rvoir la couleur */}
+          {attributes}
+        </div>
+        </div>
+            </>)        }))
+    }
+}
 export default ItemViewer;
