5cfd6753360c58b7c6fc867e407b108bf99478f5
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2020-03-25 21:14:12 +0100

    update cors rules

--- DiabloII.Items.Api/Startup.cs
+++ DiabloII.Items.Api/Startup.cs
@@ -20,7 +20,7 @@ namespace DiabloII.Items.Api
     {
         public Startup(IConfiguration configuration)
         {
-			ItemsGenerator.Generate();
+			//ItemsGenerator.Generate();
             Configuration = configuration;
         }
 
@@ -67,6 +67,11 @@ namespace DiabloII.Items.Api
             //    var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             //    context.Database.Migrate();
             //}
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
 
             app.UseSwagger();
             app.UseSwaggerUI(swagger =>
@@ -80,16 +85,15 @@ namespace DiabloII.Items.Api
 
            
             app.UseCors(builder => builder  // UseCors doit être au dessus de UseMvc;
-                .AllowAnyOrigin()
+                .WithOrigins
+                (
+                    "http://localhost:3000", // Local
+                    "https://diablo-2-enriched-documentation.netlify.com/" // Production
+                )
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials());
             app.UseMvc();
-            //app.UseCors(options => options.WithOrigins(
-            //    "https://localhost:3000", // local
-            //    "https://diablo-2-enriched-documentation.netlify.com/" // prod
-            //).AllowAnyMethod());
-
         }
     }
 }
