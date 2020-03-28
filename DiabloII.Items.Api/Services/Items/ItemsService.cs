4abeda37fa9f9f09fe7274672e29f5f3ed9ca3a6
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2020-03-28 13:06:57 +0100

    queryable extensions

--- DiabloII.Items.Api/Services/Items/ItemsService.cs
+++ DiabloII.Items.Api/Services/Items/ItemsService.cs
@@ -3,13 +3,9 @@ using DiabloII.Items.Api.DbContext.Items.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using DiabloII.Items.Api.Helpers;
 using DiabloII.Items.Api.Queries;
 using EFCore.BulkExtensions;
-using Microsoft.EntityFrameworkCore.Query;
-using Microsoft.EntityFrameworkCore.Query.Internal;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace DiabloII.Items.Api.Services.Items
 {
@@ -43,33 +39,5 @@ namespace DiabloII.Items.Api.Services.Items
                 (EnumerableHelpers.IsNullOrEmpty(query.SubCategories) ||
                  query.SubCategories.Contains(unique.SubCategory)))
             .ToList();
-    }
-
-    public static class IQueryableExtensions
-    {
-        private static readonly TypeInfo QueryCompilerTypeInfo = typeof(QueryCompiler).GetTypeInfo();
-
-        private static readonly FieldInfo QueryCompilerField = typeof(EntityQueryProvider).GetTypeInfo().DeclaredFields.First(x => x.Name == "_queryCompiler");
-
-        private static readonly FieldInfo QueryModelGeneratorField = QueryCompilerTypeInfo.DeclaredFields.First(x => x.Name == "_queryModelGenerator");
-
-        private static readonly FieldInfo DataBaseField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
-
-        private static readonly PropertyInfo DatabaseDependenciesField = typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
-
-        public static string ToSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
-        {
-            var queryCompiler = (QueryCompiler)QueryCompilerField.GetValue(query.Provider);
-            var modelGenerator = (QueryModelGenerator)QueryModelGeneratorField.GetValue(queryCompiler);
-            var queryModel = modelGenerator.ParseQuery(query.Expression);
-            var database = (IDatabase)DataBaseField.GetValue(queryCompiler);
-            var databaseDependencies = (DatabaseDependencies)DatabaseDependenciesField.GetValue(database);
-            var queryCompilationContext = databaseDependencies.QueryCompilationContextFactory.Create(false);
-            var modelVisitor = (RelationalQueryModelVisitor)queryCompilationContext.CreateQueryModelVisitor();
-            modelVisitor.CreateQueryExecutor<TEntity>(queryModel);
-            var sql = modelVisitor.Queries.First().ToString();
-
-            return sql;
-        }
     }
 }
