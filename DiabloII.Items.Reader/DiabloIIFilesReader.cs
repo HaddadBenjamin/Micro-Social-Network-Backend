09d6f92a8a4f5c29a2ac6b85ef4c0fb229923c80
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2020-03-31 17:20:20 +0200

    unique images

--- DiabloII.Items.Reader/DiabloIIFilesReader.cs
+++ DiabloII.Items.Reader/DiabloIIFilesReader.cs
@@ -572,7 +572,7 @@ namespace DiabloII.Items.Reader
                         // Stats
                         StrengthRequired = (itemCategory?.StrengthRequired * (requirementPercent + 100)) / 100,
                         DexterityRequired = (itemCategory?.DexterityRequired * (requirementPercent + 100)) / 100,
-                        ImageName = itemCategory?.ImageName ?? GetItemImageName(type, name)
+                        ImageName = GetItemImageName(type, name, itemCategory, itemData)
                     };
                 })
                 .Where(ItemFilter)
@@ -865,7 +865,7 @@ namespace DiabloII.Items.Reader
             return $"{firstChar}{value}";
         }
 
-        private static string GetItemImageName(string itemType, string itemName)
+        private static string GetItemImageName(string itemType, string itemName, ItemCategoryRecord itemCategory, string[] itemData)
         {
             var itemTypeToItemImageName = new Dictionary<string, string>
             {
@@ -889,8 +889,13 @@ namespace DiabloII.Items.Reader
                 {"Kharon of Hades", "bluecharm.gif"},
             };
 
-            return itemTypeToItemImageName.GetValueOrDefault(itemType) ??
-                   itemNameToItemImageName.GetValueOrDefault(itemName);
+            var uniqueImage = itemData[52]?.Replace("\r", string.Empty);
+            uniqueImage = uniqueImage != string.Empty ? uniqueImage : null;
+
+            return  uniqueImage ??
+                    itemCategory?.ImageName ??
+                    itemTypeToItemImageName.GetValueOrDefault(itemType) ??
+                    itemNameToItemImageName.GetValueOrDefault(itemName);
         }
 
         private static string GetItemSubCategory(string itemType)
