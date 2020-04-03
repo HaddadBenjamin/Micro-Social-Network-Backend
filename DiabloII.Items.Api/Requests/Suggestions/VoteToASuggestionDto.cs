713127119e2094d5988a313b3eb61642c6d3c2a9
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2020-04-03 15:27:35 +0200

    remove duplicates

--- DiabloII.Items.Api/Requests/Suggestions/VoteToASuggestionDto.cs
+++ DiabloII.Items.Api/Requests/Suggestions/VoteToASuggestionDto.cs
@@ -10,13 +10,4 @@ namespace DiabloII.Items.Api.Requests.Suggestions
         
         public string Ip { get; set; }
     }
-
-    public class CommentASuggestionDto
-    {
-        public Guid SuggestionId { get; set; }
-
-        public string Comment { get; set; }
-
-        public string Ip { get; set; }
-    }
 }
