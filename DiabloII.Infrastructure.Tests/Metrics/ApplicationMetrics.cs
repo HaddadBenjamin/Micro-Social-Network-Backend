547998e2554ae26f5aa9bc70d1c219c099f6a359
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2020-04-15 15:32:46 +0200

    application metrics

---
+++ DiabloII.Infrastructure.Tests/Metrics/ApplicationMetrics.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DiabloII.Infrastructure.Tests.Metrics
+{
+    [Ignore("Those information are not relative to tests, however I need them for a personal uses.")]
+    [TestFixture]
+    public class ApplicationMetrics
+    {
+        [Test]
+        public void CountCSharpFilesVolumetry()
+        {
+            var csharpFiles = Directory.EnumerateFiles(@"C:\Users\hadda\OneDrive\Bureau\Travaux\Diablo-II-Items", "*.cs", SearchOption.AllDirectories);
+            var csharpFilesCount = csharpFiles.Count();
+            var csharpLinesCount = csharpFiles.Select(file => File.ReadAllLines(file).Length).Sum();
+        }
+    }
+}
