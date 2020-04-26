41a029c2e5d12934a30157382a56922c79eb6b98
Benjamin Haddad (maison) <haddad.b.pro@gmail.com>
2020-04-26 17:45:30 +0200

    fix service

--- DiabloII.Domain/Helpers/EnumerationFlagsHelpers.cs
+++ DiabloII.Domain/Helpers/EnumerationFlagsHelpers.cs
@@ -4,17 +4,24 @@ using System.Linq;
 
 namespace DiabloII.Domain.Helpers
 {
-    public static class EnumerationFlagsHelpers
-    {
-        public static int ToInteger<EnumerationType>(IEnumerable<EnumerationType> enumerations) where EnumerationType : struct, IConvertible => enumerations
-            .Select(enumeration => (int)(object)enumeration)
-            .Aggregate((flagsSum, flagToAdd) => flagsSum | flagToAdd);
+	public static class EnumerationFlagsHelpers
+	{
+		public static int ToInteger<EnumerationType>(IEnumerable<EnumerationType> enumerations) where EnumerationType : struct, IConvertible
+		{
+			var enumerationValues = enumerations
+				.Select(enumeration => (int) (object) enumeration)
+				.ToList();
 
-        public static IEnumerable<string> ToStrings<EnumerationType>(int enumerationValue)
-            where EnumerationType : struct, IConvertible =>
-            Enum.GetValues(typeof(EnumerationType))
-                .Cast<EnumerationType>()
-                .Where(enumeration => (enumerationValue & (int)(object)enumeration) != 0)
-                .Select(enumeration => enumeration.ToString());
-    }
+            return enumerationValues.Any()
+                ? enumerationValues.Aggregate((flagsSum, flagToAdd) => flagsSum | flagToAdd)
+                : 0;
+		}
+
+		public static IEnumerable<string> ToStrings<EnumerationType>(int enumerationValue)
+			where EnumerationType : struct, IConvertible =>
+			Enum.GetValues(typeof(EnumerationType))
+				.Cast<EnumerationType>()
+				.Where(enumeration => (enumerationValue & (int)(object)enumeration) != 0)
+				.Select(enumeration => enumeration.ToString());
+	}
 }
